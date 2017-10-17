b9ee8e69c9fd1cd1c36f4d60b830c8e382eaba7d
acouprie <acouprie@hotmail.fr>
2017-10-18 00:11:36 +0200

    add 2 parts

--- getQuotidien.rb
+++ getQuotidien.rb
@@ -4,21 +4,29 @@ require 'nokogiri'
 
 date = Time.now
 day = date.day
+raw_month = date.month
 year = date.year
-error = "Error - Abort"
+monthString = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
 
-url = "https://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-premiere-partie-#{day}-octobre-#{year}.html"
+month = monthString[raw_month-1]
 
-html = %x(curl #{url} > quotidien.html)
+url_first = "https://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-premiere-partie-#{day}-#{month}-#{year}.html"
+puts url_first
+url_second = "https://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-deuxieme-partie-#{day}-#{month}-#{year}.html"
+puts url_second
 
-if html.empty?
-  puts error
-end
-page = Nokogiri::HTML(open("quotidien.html"))
-doc = page.xpath('//div[@id="zonePlayer"]/@data-src').first.value[2..-1]
+%x(curl #{url_first} > quotidien-first.html)
+%x(curl #{url_second} > quotidien-second.html)
 
-puts "Opening URL : #{doc}"
-%x(firefox #{doc})
+first = Nokogiri::HTML(open("quotidien-first.html")).xpath('//div[@id="zonePlayer"]/@data-src').first.value[2..-1]
+
+second = Nokogiri::HTML(open("quotidien-second.html")).xpath('//div[@id="zonePlayer"]/@data-src').first.value[2..-1]
+
+puts "Opening first part URL : #{first}"
+%x(firefox #{first})
+puts "Opening second part URL : #{second}"
+%x(firefox #{second})
 
 # cleaning
-%x(rm quotidien.html)
+%x(rm quotidien-first.html)
+%x(rm quotidien-second.html)
